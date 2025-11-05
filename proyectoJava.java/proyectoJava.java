188ba690a3a0c8ac6175fa119b3f0ceecfd05e78
claudia <claudia.rt052@alumnos.alborfp.com>
2025-11-05 19:36:39 +0100

    ejercicio80

---
+++ proyectoJava.java/proyectoJava.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+public class main {
+
+
+public static void main (String [] args){
+
+    Scanner leer = new Scanner (System.in);
+
+    //EJERCICIO 10
+    /* 
+    int contador = 0;
+    int num = 1;
+
+    System.out.println("Ingrese un numero");
+    num = leer.nextInt();
+
+    for (contador = num; contador <= num + 5; contador ++){
+        System.out.println(contador + "el cuadrado es " + (contador * contador) + "el cubo es" + (contador * contador * contador));
+    }
+
+*/
+
+//EJERCICIO 12
+/* 
+int positivo;
+int negativo;
+int cantidad = 10;
+int num;
+int neutro;
+
+
+    for (int i; i < -1; i = cantidad){
+    System.out.println("Escribe los numeros");
+    num = leer.nextInt();
+
+
+    if (num >= 1){
+        System.out.println("El numero es positivo");
+    }else if (num < 0)
+        System.out.println("El numero es negativo");
+    else {
+        System.out.println("El numero es 0");
+   }
+
+}
+    System.out.println("La cantidad de numeros postivos es" + (positivos));
+    System.out.println("La cantidad de numeros postivos es" + (negativos));
+*/
+
+
+//EJERCICIO 14
+/*
+int base;
+int exponente;
+int resultado;
+int i;
+System.out.println("Introduce la base");
+base = leer.nextInt();
+System.out.println("Introduce el exponente");
+exponente = leer.nextInt();
+
+    if (exponente <= 0){
+        System.out.println("El numero del exponente debe ser positivo");
+    }else {
+        System.out.println("Las potencias son");
+        resultado = 1.0;
+        for (int j = 0; j < i; j ++){
+            resultado * = base;
+        }
+    }
+        Sysem.out.println(base + " elevado a " + i + "es : " + resultado);
+ */
+
+
+//EJERCICIO 15
+/* 
+int num;
+int primo;
+primo = (num/num = 1) & (num/1 = num)
+int NOprimo;
+
+System.out.println("Introduce el numero");
+num = leer.nextInt();
+
+    if (primo >= 0){
+        System.out.println("El numero es primo");
+    }else {
+        System.out.println("El numero no es primo");
+    }
+*/
+
+//EJERCICIO 16
+
+
+
+
+
+
+
+
+}
+}
+
